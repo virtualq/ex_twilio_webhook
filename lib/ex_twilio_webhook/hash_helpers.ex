7abfcd10c31cea01c7987ddf8351b1ff1768f1ee
Karol Moroz <karol@moroz.dev>
2023-01-30 14:04:50 +0100

    Port build_url_with_standard_port from Rust

--- lib/ex_twilio_webhook/hash_helpers.ex
+++ lib/ex_twilio_webhook/hash_helpers.ex
@@ -3,4 +3,50 @@ defmodule ExTwilioWebhook.HashHelpers do
     digested = :crypto.mac(:hmac, :sha, key, data)
     Base.encode64(digested)
   end
+
+  def get_expected_twilio_signature(auth_token, url, params)
+      when is_binary(auth_token) and is_binary(url) do
+    data = url <> normalize_data(params)
+    hmac_sha1_base64(auth_token, data)
+  end
+
+  defp normalize_data(string) when is_binary(string), do: string
+  defp normalize_data(list) when is_list(list), do: Enum.join(list)
+
+  defp normalize_data(map) when is_map(map) do
+    map
+    |> Enum.map(fn {key, value} -> [to_string(key), to_string(value)] end)
+    |> Enum.join()
+  end
+
+  def build_url_with_standard_port(url) when is_binary(url) do
+    parsed = URI.parse(url)
+
+    normalized_host =
+      case parsed.host do
+        nil -> ""
+        host -> "#{host}:#{parsed.port}"
+      end
+
+    normalized_query =
+      case parsed.query do
+        nil -> ""
+        query -> "?#{query}"
+      end
+
+    normalized_fragment =
+      case parsed.fragment do
+        nil -> ""
+        fragment -> "##{fragment}"
+      end
+
+    Enum.join([
+      parsed.scheme,
+      "://",
+      normalized_host,
+      parsed.path,
+      normalized_query,
+      normalized_fragment
+    ])
+  end
 end
--- test/ex_twilio_webhook/hash_helpers_test.exs
+++ test/ex_twilio_webhook/hash_helpers_test.exs
@@ -13,4 +13,17 @@ defmodule ExTwilioWebhook.HashHelpersTest do
       assert actual == expected
     end
   end
+
+  describe "build_url_with_standard_port/1" do
+    @input "https://api.example.com/twilio/conference_status.xml?waiter_id=42#rc=5&rp=all&sni=y"
+
+    test "adds port :443 to https: urls" do
+      actual = HashHelpers.build_url_with_standard_port(@input)
+
+      expected =
+        "https://api.example.com:443/twilio/conference_status.xml?waiter_id=42#rc=5&rp=all&sni=y"
+
+      assert actual == expected
+    end
+  end
 end
