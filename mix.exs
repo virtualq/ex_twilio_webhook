ff3290c130de4fe9e9b53b177f594ebfe3434824
Karol Moroz <k.j.moroz@gmail.com>
2023-02-03 18:26:54 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExTwilioWebhook.MixProject do
   def project do
     [
       app: :ex_twilio_webhook,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
